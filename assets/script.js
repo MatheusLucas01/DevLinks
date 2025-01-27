274cfcf7ecf809c800794326cf0544ac19df8fc7
MatheusLucas01 <matheuslucasdesouza22@gmail.com>
2025-01-27 13:27:40 -0300

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// Quando der o clique no switch
+function toggleMode() {
+  const html = document.documentElement // Representação do documento em formato JS
+  html.classList.toggle("light") // Ferramenta pronta
+
+  //Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/foto.jpg")
+    img.setAttribute("alt", "imagem ao fundo")
+  } else {
+    // se tiver sem light mode, mantém a imagem normal
+    img.setAttribute("src", "./assets/Avatar.png")
+    img.setAttribute("src", "imagem de perfil")
+  }
+}
+
+/* Exemplo com condição
+Se a lista de classe contém
+  if (html.classList.contains('light')) {
+    html.classList.remove('light') // Irá remover o light
+  } else{
+   html.classList.add('light') // Irá adicionar o light
+  }
+} */
