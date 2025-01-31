70b5eb71cf6859162a1ed13d600bdb7b84569bd9
MatheusLucas01 <matheuslucasdesouza22@gmail.com>
2025-01-31 13:34:47 -0300

    Alterações nas imgs e background

--- assets/script.js
+++ assets/script.js
@@ -9,20 +9,12 @@ function toggleMode() {
   // Substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "../assets/img/foto.jpg")
+    img.setAttribute("src", "../assets/img/foto2.png")
     img.setAttribute("alt", "Imagem de perfil")
   } else {
     // se tiver sem light mode, mantém a imagem normal
-    img.setAttribute("src", "../assets/img/perfil.png")
+    img.setAttribute("src", "../assets/img/foto2.png")
     img.setAttribute("alt", "imagem de perfil")
   }
 }
 
-/* Exemplo com condição
-Se a lista de classe contém
-  if (html.classList.contains('light')) {
-    html.classList.remove('light') // Irá remover o light
-  } else{
-   html.classList.add('light') // Irá adicionar o light
-  }
-} */
